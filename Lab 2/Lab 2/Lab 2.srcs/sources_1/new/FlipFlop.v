7cd6ccd2746292f6ba9b37cb9def982a8a0362e0
cindie-le <cvle104@gmail.com>
2020-11-01 01:41:44 -0700

    removed some test bench cases and comments

--- Lab 2/Lab 2/Lab 2.srcs/sources_1/new/FlipFlop.v
+++ Lab 2/Lab 2/Lab 2.srcs/sources_1/new/FlipFlop.v
@@ -7,16 +7,12 @@ module FlipFlop(
     output reg [7:0] q
     );
     
-   // reg [7:0] out;
-    //
     always @(posedge clk) //Posedge triggers the block on the positive edge of a clock signal, negedge triggers negative edge
     begin
         if( reset == 1'b1)
-            //out <= 1'b0;
             q <= 1'b0;
         else
-            //out <= d;
             q <= d;
      end
-    // assign q = out;
+
 endmodule
